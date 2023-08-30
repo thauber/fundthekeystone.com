1eba70ecddc6e2b207c9c7846b65fc3f6421bb93
Tony Hauber <thauber@gmail.com>
2023-08-29 21:36:31 -0500

    feat: added og tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
 import GA4 from '@/components/tracking/GA4'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ParallaxProvider } from 'react-scroll-parallax'
+import Head from 'next/head'
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
+    <Head>
+      <title>Fund The Keystone - Elkader, Iowa</title>
+      <meta property="og:title" content="Fund the Keystone: Elkader, Iowa" key="title" />
+      <meta property="og:description" content="Help us build a bridge to the future. Fund the Keystone." key="description" />
+      <meta property="og:image" content="https://fundthekeystone.com/imagges/thumb.png" key="image" />
+    </Head>
     <Component {...pageProps} />
     <GA4 />
   </>
