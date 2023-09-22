c8b6216586dd333f693cdb00e1f488506eb4d2ee
Tony Hauber <thauber@gmail.com>
2023-09-22 12:21:54 -0500

    fix: adding background video for iphone

--- components/Header.tsx
+++ components/Header.tsx
@@ -19,29 +19,31 @@ const Header = ({onDonate}:{onDonate:()=>void}) => {
   }, []);
 
   // Calculate the font size and color based on scroll position
-  const fontSize = scrollY > breakpoint ? "text-sm md:text-xl" : "text-[12vw] leading-[12vw] lg:text-[14vw]  lg:leading-[14vw]";
-  const bgColor = scrollY > breakpoint ? "bg-field" : "bg-transparent";
-  const textColor = scrollY > breakpoint ? "text-white" : "text-bright drop-shadow-lg";
-  const height = scrollY > breakpoint ? "h-16" : "h-full";
-  const top = scrollY > breakpoint ? "" : "top-10";
-  const right = scrollY > breakpoint ? "right-2" : "right-10";
-  const social = scrollY > breakpoint ? "hidden md:block" : "";
-  const donate = scrollY > breakpoint ? "mx-4 " : "text-2xl my-1 mx-4 md:text-4xl my-2 mx-8 ";
+  const isHeader = scrollY > breakpoint;
+  const fontSize = isHeader ? "text-sm md:text-xl" : "text-[21vw] leading-[21vw] md:text-[13vw] md:leading-[14vw] lg:text-[14vw]  lg:leading-[14vw]";
+  const bgColor = isHeader ? "bg-field" : "bg-transparent";
+  const textColor = isHeader ? "text-white" : "text-bright drop-shadow-lg";
+  const height = isHeader ? "h-16" : "h-full";
+  const top = isHeader ? "" : "top-10";
+  const right = isHeader ? "right-2" : "right-10";
+  const social = isHeader ? "hidden md:block" : "";
+  const donate = isHeader ? "mx-4 " : "text-2xl my-1 mx-4 md:text-4xl my-2 mx-8 ";
 
   return (
     <header
       className={`z-20 w-full p-4 fixed top-0 transition-all duration-200 ease-in-out ${bgColor} ${textColor} ${height} flex justify-between items-center`}
     >
       {scrollY <= breakpoint && <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0">
-        <video autoPlay loop muted style={{ position: 'absolute', width: '100%', height: '100%', objectFit: 'cover' }}>
-          <source src="/video/hero-hq.webm" type="video/mp4" />
+        <video autoPlay playsInline loop muted style={{ position: 'absolute', width: '100%', height: '100%', objectFit: 'cover' }}>
+          <source src="/video/hero-hq.webm" type="video/webm" />
+          <source src="/video/hero-hq.mp4" type="video/mp4" />
         </video>
       </div>}
       <div className="relative z-10 w-full break-words opacity-90">
         <h1 className={`${fontSize} font-display transition-all duration-200 ease-in-out w-full`}>
-          <span>Fund</span>{scrollY <= breakpoint && <br />}
-          <span>The</span>{scrollY <= breakpoint && <br />}
-          <span>Keystone</span>
+          <span>Fund</span>{!isHeader && <br />}
+          <span>The</span>{!isHeader && <br />}
+          <span>Key</span>{!isHeader && <br className="md:hidden"/>}<span>stone</span>
         </h1>
       </div>
       <div className={`absolute z-10 flex h-13 items-center ${top} ${right}`}>
